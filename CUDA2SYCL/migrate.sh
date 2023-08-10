ba1fae7d78c277b4b95f6fc5a91d9280a67a532e
martin <martin@CGGcluster>
2023-08-10 17:35:08 +0200

    CUDA->SYCL migration: using a list of (JIT) build directories generated when running the StyleGAN code rather than using hardcoded paths, allowed 0-epochs training for just building the modules and exiting, better error handling

--- CUDA2SYCL/migrate.sh
+++ CUDA2SYCL/migrate.sh
@@ -7,17 +7,13 @@
 # enter the environment with StyleGAN dependencies
 #   conda activate stylegan3
 # generate compilation database for each module by running train.py (in the project root directory)
-#   python train.py --outdir=~/training-runs --cfg=stylegan3-t --data=/projects/ImageDatasets/Other/AFHQv2/afhq_v2.zip --gpus=1 --batch=32 --gamma=8.2 --mirror=1
-#   # Note: --gamma should be different for single-GPU training, but we just want to compile the modules, then kill the training with Ctrl+C
-# Finally run one of the following commands (depending on wheter c2s from Base Toolkit or SYCLomatic should be used) in the current directory:
+#   python train.py --outdir=~/training-runs --cfg=stylegan3-t --data=/projects/ImageDatasets/Other/AFHQv2/afhq_v2.zip --gpus=1 --batch=4 --gamma=8.2 --metrics=none --kimg=0 | grep 'CHECK THE BUILD DIR FOR compile_commands.json:' | sed 's/CHECK THE BUILD DIR FOR compile_commands.json: //' > CUDA2SYCL/cuda_builddirs.txt
+#   # Note: --gamma should be different for single-GPU training, but we just want to compile the modules, then no training is run
+# cuda_builddirs.txt now says where the modules and their compilation databases are located (each directory on one line)
+# Finally run this script. Use one of the following commands (depending on wheter c2s from Base Toolkit or SYCLomatic should be used) in the current directory:
 #   ( . /opt/intel/oneapi/setvars.sh && ./migrate.sh )
 #   ( export PATH=~/intel_hackathon/SYCLomatic_2023-08-09/bin:"$PATH" && ./migrate.sh )
-# the migrated source code will appear in torch_utils/ops as individual directories named as the module + c2s version
-
-# where the modules and their compilation databases are located (the paths are printed by this patched StyleGAN when the modules is built)
-bias_act_builddir="/home/martin/.cache/torch_extensions/bias_act_plugin/3cb576a0039689487cfba59279dd6d46-nvidia-rtx-a6000"
-filtered_lrelu_builddir="/home/martin/.cache/torch_extensions/filtered_lrelu_plugin/2e9606d7cf844ec44b9f500eaacd35c0-nvidia-rtx-a6000"
-upfirdn2d_builddir="/home/martin/.cache/torch_extensions/upfirdn2d_plugin/7edf9e6a584689218f8aa5314b3a0356-nvidia-rtx-a6000"
+# The migrated source code will appear in torch_utils/ops as individual directories named as the module + c2s version.
 
 # where to copy the directory with migrated files
 outdir="$(pwd)/../torch_utils/ops"
@@ -28,10 +24,14 @@ migrate() {
         mv compile_commands.json compile_commands.json.ori
         grep -v '"output"' compile_commands.json.ori > compile_commands.json
     fi
-    c2s --gen-helper-function --out-root dpct_out_$c2s_version -p .  >migrate.stdout.log 2>migrate.stderr.log
-    #echo $? > c2s.exitcode.log
     module_name=$(ls | grep .h$ | sed 's/\.h$//') # assuming the build directory contains a single header named as the module (good enough for SytleGAN3)
-    cp -r dpct_out_$c2s_version "$outdir"/${module_name}_dpct_out_$c2s_version
+    module_outdir="$outdir"/"${module_name}"_dpct_out_"$c2s_version"
+    c2s --gen-helper-function --out-root "$module_outdir" -p .  >migrate.stdout.log 2>migrate.stderr.log || ret=$?
+    if [ $ret -ne 0 ]; then
+        cat migrate.stdout.log migrate.stderr.log
+        exit $ret
+    fi
+    mv migrate.stdout.log migrate.stderr.log "$module_outdir"/
 }
 
 if ! which c2s; then
@@ -44,19 +44,12 @@ c2s_version=$(c2s --version | sed -n 's/.*version //;s/. Codebase:(/_/;s/)$//p')
 set -x
 set -e
 
-(
-    cd "$bias_act_builddir"
-    migrate
-)
-
-(
-    cd $filtered_lrelu_builddir
-    migrate
-)
-
-(
-    cd $upfirdn2d_builddir
-    migrate
-)
+while read builddir; do # foreach builddir in file cuda_builddirs.txt
+    echo "Mirgating $builddir"
+    (
+        cd "$builddir"
+        migrate
+    )
+done <cuda_builddirs.txt
 
 echo Migration done
