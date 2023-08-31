bias_act: WIP porting to SYCL: bias_act runs on XPU, but gives incorrect results (different on subsequent runs -> likely reading uninitialized memory)

torch_utils/custom_ops.py
@@ -16,6 +16,8 @@ import subprocess
 import uuid
 
 import torch
+import intel_extension_for_pytorch as ipex
+
 import torch.utils.cpp_extension
 from torch.utils.file_baton import FileBaton
 
@@ -43,7 +45,7 @@ def _find_compiler_bindir():
 #----------------------------------------------------------------------------
 
 def _get_mangled_gpu_name():
-    name = torch.cuda.get_device_name().lower()
+    name = ipex.xpu.get_device_name(0).lower()
     out = []
     for c in name:
         if re.match('[a-z0-9_-]+', c):
@@ -63,7 +65,7 @@ def hooked_subprocess_run(command, **kwargs):
 subprocess.run = hooked_subprocess_run
 
 #----------------------------------------------------------------------------
-# Main entry point for compiling and loading C++/CUDA plugins.
+# Main entry point for compiling and loading C++/XPU plugins.
 
 _cached_plugins = dict()
 
@@ -95,12 +97,6 @@ def get_plugin(module_name, sources, headers=None, source_dir=None, **build_kwar
                 raise RuntimeError(f'Could not find MSVC/GCC/CLANG installation on this computer. Check _find_compiler_bindir() in "{__file__}".')
             os.environ['PATH'] += ';' + compiler_bindir
 
-        # Some containers set TORCH_CUDA_ARCH_LIST to a list that can either
-        # break the build or unnecessarily restrict what's available to nvcc.
-        # Unset it to let nvcc decide based on what's available on the
-        # machine.
-        os.environ['TORCH_CUDA_ARCH_LIST'] = ''
-
         # Incremental build md5sum trickery.  Copies all the input source files
         # into a cached build directory under a combined md5 digest of the input
         # source files.  Copying is done only if the combined digest has changed.
@@ -127,7 +123,7 @@ def get_plugin(module_name, sources, headers=None, source_dir=None, **build_kwar
 
             # Select cached build directory name.
             source_digest = hash_md5.hexdigest()
-            build_top_dir = torch.utils.cpp_extension._get_build_directory(module_name, verbose=verbose_build) # pylint: disable=protected-access
+            build_top_dir = torch.xpu.cpp_extension._get_build_directory(module_name, verbose=verbose_build) # pylint: disable=protected-access
             cached_build_dir = os.path.join(build_top_dir, f'{source_digest}-{_get_mangled_gpu_name()}')
 
             if not os.path.isdir(cached_build_dir):
@@ -146,10 +142,10 @@ def get_plugin(module_name, sources, headers=None, source_dir=None, **build_kwar
             cached_sources = [os.path.join(cached_build_dir, os.path.basename(fname)) for fname in sources]
 
             #subprocess.run(command, stdout=stdout_fileno if verbose else subprocess.PIPE, stderr=subprocess.STDOUT, cwd=build_directory, check=True, env=env)
-            torch.utils.cpp_extension.load(name=module_name, build_directory=cached_build_dir,
+            torch.xpu.cpp_extension.load(name=module_name, build_directory=cached_build_dir,
                 verbose=verbose_build, sources=cached_sources, **build_kwargs)
         else:
-            torch.utils.cpp_extension.load(name=module_name, verbose=verbose_build, sources=sources, **build_kwargs)
+            torch.xpu.cpp_extension.load(name=module_name, verbose=verbose_build, sources=sources, **build_kwargs)
 
         # Load.
         module = importlib.import_module(module_name)