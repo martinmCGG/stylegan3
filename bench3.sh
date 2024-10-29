615186bc54e4e73acd8b8d9006a3788ac4060d76
martinm <user@Z790P-Ubuntu-2204>
2024-10-29 14:43:07 +0100

    bench3: running all 3 architectures without profilier

--- bench3.sh
+++ bench3.sh
@@ -43,12 +43,12 @@ fi
 
 profile() {
     # profile the given command using VTune
-    VTUNE_BIN=/opt/intel/oneapi/vtune/2024.3/bin64/vtune
+    #VTUNE_BIN=/opt/intel/oneapi/vtune/2024.3/bin64/vtune
     #VTUNE_BIN="$HOME"/Intel_VTune_Profiler_2024.1.0/bin64/vtune
-    $VTUNE_BIN -collect gpu-hotspots -knob profiling-mode=source-analysis --app-working-dir="$HOME"/stylegan3 -- "$@"
+    #$VTUNE_BIN -collect gpu-hotspots -knob profiling-mode=source-analysis --app-working-dir="$HOME"/stylegan3 -- "$@"
     #$VTUNE_BIN -collect gpu-hotspots --app-working-dir="$HOME"/stylegan3 -- "$@"
     #$VTUNE_BIN -collect gpu-offload --app-working-dir="$HOME"/stylegan3 -- "$@"
-    return
+    #return
 
     # or just run it directly
     #"$@"
@@ -71,10 +71,10 @@ FRAME_COUNT=32
 #export DNNL_VERBOSE=1
 
 # stylegan3-r (uses `bias_act` and `filtered_lrelu`): 
-#profile python gen_video.py --output=benchmark3r.mp4 --trunc=1 --seeds=2,5 --w-frames=$FRAME_COUNT --network=https://api.ngc.nvidia.com/v2/models/nvidia/research/stylegan3/versions/1/files/stylegan3-r-afhqv2-512x512.pkl --preheat=True
+profile python gen_video.py --output=benchmark3r.mp4 --trunc=1 --seeds=2,5 --w-frames=$FRAME_COUNT --network=https://api.ngc.nvidia.com/v2/models/nvidia/research/stylegan3/versions/1/files/stylegan3-r-afhqv2-512x512.pkl --preheat=True
 
 # stylegan3-t (also uses `bias_act` and `filtered_lrelu`, but faster - different filter sizes?): 
-#profile python gen_video.py --output=benchmark3t.mp4 --trunc=1 --seeds=2,5 --w-frames=$FRAME_COUNT --network=https://api.ngc.nvidia.com/v2/models/nvidia/research/stylegan3/versions/1/files/stylegan3-t-afhqv2-512x512.pkl --preheat=True
+profile python gen_video.py --output=benchmark3t.mp4 --trunc=1 --seeds=2,5 --w-frames=$FRAME_COUNT --network=https://api.ngc.nvidia.com/v2/models/nvidia/research/stylegan3/versions/1/files/stylegan3-t-afhqv2-512x512.pkl --preheat=True
 
 # stylegan2 (uses `bias_act` and `upfirdn2d`)
 profile python gen_video.py --output=benchmark2.mp4 --trunc=1 --seeds=2,10 --w-frames=$FRAME_COUNT --network=https://api.ngc.nvidia.com/v2/models/nvidia/research/stylegan2/versions/1/files/stylegan2-afhqv2-512x512.pkl --preheat=True
