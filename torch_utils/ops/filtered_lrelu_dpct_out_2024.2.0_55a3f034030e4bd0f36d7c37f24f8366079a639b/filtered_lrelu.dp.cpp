92993a4c716551ffaa7e5360547da9c1a3b74a08
Igor Vorobtsov <ivorobts@sprpvc.imu.intel.com>
2025-03-21 21:19:57 +0100

    Revert "Update filtered_lrelu.dp.cpp to compile with PyTorch 2.3.1"
    This reverts commit 8f9c6eb040f7b378366dfd820a45ae7ac967ed7c.

--- torch_utils/ops/filtered_lrelu_dpct_out_2024.2.0_55a3f034030e4bd0f36d7c37f24f8366079a639b/filtered_lrelu.dp.cpp
+++ torch_utils/ops/filtered_lrelu_dpct_out_2024.2.0_55a3f034030e4bd0f36d7c37f24f8366079a639b/filtered_lrelu.dp.cpp
@@ -13,7 +13,6 @@
 #include <torch/extension.h>
 #include <c10/util/Half.h>
 #include <ipex.h>
-#include <c10/xpu/XPUStream.h>
 
 //------------------------------------------------------------------------
 // Helpers.
@@ -1955,8 +1954,10 @@ template <class T, class index_t, bool signWrite, bool signRead, int SH,
     }
 
     // Launch filter setup kernel.
-    c10::xpu::XPUStream stream = c10::xpu::getCurrentXPUStream();
-    auto& queue = stream.queue();
+    auto device_type = c10::DeviceType::XPU;
+    c10::impl::VirtualGuardImpl impl(device_type);
+    c10::Stream c10_stream = impl.getStream(c10::Device(device_type));
+    auto& queue = xpu::get_queue_from_stream(c10_stream);
     /*
     DPCT1049:0: The work-group size passed to the SYCL kernel may exceed the
     limit. To get the device limit, query info::device::max_work_group_size.
@@ -2203,8 +2204,10 @@ void run_filtered_lrelu_act_kernel(filtered_lrelu_act_kernel_params &p) try {
     gz = std::min(gz, gmax);
 
     // Launch.
-    c10::xpu::XPUStream stream = c10::xpu::getCurrentXPUStream();
-    auto& queue = stream.queue();
+    auto device_type = c10::DeviceType::XPU;
+    c10::impl::VirtualGuardImpl impl(device_type);
+    c10::Stream c10_stream = impl.getStream(c10::Device(device_type));
+    auto& queue = xpu::get_queue_from_stream(c10_stream);
     /*
     DPCT1049:2: The work-group size passed to the SYCL kernel may exceed the
     limit. To get the device limit, query info::device::max_work_group_size.
